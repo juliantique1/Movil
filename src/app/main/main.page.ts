ae5defb24351506a45647346a4ede4baa9da3634
unknown <juliantique61@gmail.com>
2024-11-24 22:35:36 -0500

    hs

---
+++ src/app/main/main.page.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
+})
+export class MainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
